99e3a9aad51729bb1f21fc8c095355f0bcbf7f8b
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-11-16 20:49:50 -0300

    📦 NEW: 🌟Feat. Button On Work

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,42 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+#define LIGA 1
 
 void setup() {
   lcd.begin(16, 2);
+  pinMode(LIGA, INPUT);
   // put your setup code here, to run once:
 }
 
+void inputValveDisplay(){
+  lcd.clear();
+  lcd.setCursor(6,0);
+  lcd.print("V.E.");
+  lcd.setCursor(4,1);
+  lcd.print("Acionada");
+  while (true)
+  {
+    delay(1000);
+  }
+}
+
+void buttonLigaIsPressed(){
+  if(digitalRead(LIGA) == HIGH)
+    {
+      inputValveDisplay();
+    }
+}
+
+
+void initialDisplay(){ //The inital message of programm
+  lcd.setCursor(3,0);
+  lcd.print("Pressione");
+  lcd.setCursor(6,1);
+  lcd.print("LIGA");
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  initialDisplay();
+  buttonLigaIsPressed();
 }
