89cc8292399ace6c1ec6b27b901ac216791d286c
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-11-16 21:43:56 -0300

    📦 NEW: New Sensor and Motor

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 #define LIGA 1
 #define LEDVE 8
+#define SNA 9
+#define MOTOR 10
+
 
 void setup() {
   lcd.begin(16, 2);
   pinMode(LIGA, INPUT);
   pinMode(LEDVE, OUTPUT);
+  pinMode(SNA, INPUT);
+  pinMode(MOTOR, OUTPUT);
   // put your setup code here, to run once:
 }
 
